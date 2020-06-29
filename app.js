5e4d68b8e265747af826f4aa33f4f9c61bc34296
Jusan Magno Pinto da Silva <jusanmagno@gmail.com>
2020-06-29 12:52:38 -0300

    adicionado sistema da nova api pushando rota por parque ..

--- app.js
+++ app.js
@@ -6,7 +6,28 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 const PORT = 3000;
 const HOST = "0.0.0.0";
+const Themeparks = require("themeparks");
+let status = 1;
 
+//parques
+const DisneyWorldMagicKingdom = new Themeparks.Parks.WaltDisneyWorldMagicKingdom();
+const WaltDisneyWorldHollywoodStudios = new Themeparks.Parks.WaltDisneyWorldHollywoodStudios();
+const WaltDisneyWorldAnimalKingdom = new Themeparks.Parks.WaltDisneyWorldAnimalKingdom();
+const WaltDisneyWorldEpcot = new Themeparks.Parks.WaltDisneyWorldEpcot();
+
+const UniversalStudiosFlorida = new Themeparks.Parks.UniversalStudiosFlorida();
+const UniversalIslandsOfAdventure = new Themeparks.Parks.UniversalIslandsOfAdventure();
+
+const SeaworldOrlando = new Themeparks.Parks.SeaworldOrlando();
+const BuschGardensTampa = new Themeparks.Parks.BuschGardensTampa();
+const SixFlagsDiscoveryKingdom = new Themeparks.Parks.SixFlagsDiscoveryKingdom();
+
+
+
+
+
+
+//retorna resposta como JSON
 app.get('/', (req, res) => {
     const URL = req.query.data;
 
@@ -20,6 +41,7 @@ app.get('/', (req, res) => {
 
 });
 
+//aplica stream a url de stream
 app.get('/radio', (req, res) => {
     const URL = req.query.data;
     var strem = request(URL)
@@ -37,6 +59,61 @@ app.get('/radio', (req, res) => {
     }
 
 });
+
+app.get('/park', (req, res) => {
+    
+    res.header("Access-Control-Allow-Origin", "*");
+
+    const park = req.query.park;
+
+    switch (park) {
+        case 'DisneyWorldMagicKingdom':
+            dataPark = DisneyWorldMagicKingdom;
+            break;
+        case 'WaltDisneyWorldHollywoodStudios':
+            dataPark = WaltDisneyWorldHollywoodStudios;
+            break;
+        case 'WaltDisneyWorldAnimalKingdom':
+            dataPark = WaltDisneyWorldAnimalKingdom;
+            break;
+        case 'WaltDisneyWorldEpcot':
+            dataPark = WaltDisneyWorldEpcot;
+            break;
+        case 'UniversalStudiosFlorida':
+            dataPark = UniversalStudiosFlorida;
+            break;
+        case 'UniversalIslandsOfAdventure':
+            dataPark = UniversalIslandsOfAdventure;
+            break;
+        case 'SeaworldOrlando':
+            dataPark = SeaworldOrlando;
+            break;
+        case 'BuschGardensTampa':
+            dataPark = BuschGardensTampa;
+            break;
+        case 'SixFlagsDiscoveryKingdom':
+            dataPark = SixFlagsDiscoveryKingdom;
+            break;
+        default:
+            res.json('parque nao suportado')
+            break;
+    }
+
+    // Access wait times by Promise
+    dataPark.GetWaitTimes().then((rideTimes) => {
+        res.json(rideTimes);
+    }).catch((error) => {
+        console.error(error);
+    })
+})
+
+
+app.get('/teste', (req, res) => {
+    res.render(__dirname + '/test.html');
+
+})
+
+
 
 app.listen(PORT, PORT)
 
